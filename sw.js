a9dc5af13a4b5a3f1306116861df5f008322baf1
simplebusiness26 <simplebussiness26@gmail.com>
2026-08-25 01:29:33 +0100

    Cache soundtrack for offline play

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'one-more-beat-v1';
-const ASSETS = ['./','./index.html','./styles.css','./game.js','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE = 'one-more-beat-v2-music';
+const ASSETS = ['./','./index.html','./styles.css','./game.js','./music.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
